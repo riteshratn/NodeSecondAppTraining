0d440482ed820e403f5ea4f0e1b5f0777b4b21ec
Ritesh Kumar <0923riteshkumar@gmail.com>
2023-10-20 13:48:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,146 +1,80 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-
 const express = require('express');
 const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: '0923riteshkumar@gmail.com',
-      pass: 'ekhietjloouoigak'
-    }
-  });
-  
 const path = require('path');
-const app = express();
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const initializePassport = require('./config');
+const authController = require('./controllers/authController');
+const dotenv = require('dotenv').config();
 
-const initializePassport = require('./config')
 initializePassport(
   passport,
   email => users.find(user => user.email === email),
   id => users.find(user => user.id === id)
-)
+);
 
 const users = [];
 
 const publicDirectoryPath = path.join(__dirname, 'public');
+const app = express();
+
 app.use(express.static(publicDirectoryPath));
 
-app.set('view-engine','ejs');
-app.use(express.urlencoded({extended: false}));
-app.use(flash())
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
-}))
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
 
-app.get('/', checkAuthenticated , (req,res) => {
-    res.render('index.ejs',{ name: req.user.name});
-})
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name });
+});
 
-app.get('/login', checkNotAuthenticated, (req,res) => {
-    res.render('login.ejs');
-})
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs');
+});
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  }))
+app.post('/login', checkNotAuthenticated, authController.loginUser); 
 
-app.get('/register', checkNotAuthenticated, (req,res) => {
-    res.render('register.ejs');
-})
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs');
+});
 
-// app.post('/register', checkNotAuthenticated, async (req, res) => {
-//     try {
-//       const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//       users.push({
-//         id: Date.now().toString(),
-//         name: req.body.name,
-//         email: req.body.email,
-//         password: hashedPassword
-//       })
-//       res.redirect('/login')
-//     } catch {
-//       res.redirect('/register')
-//     }
-//     console.log(users);
-//   })
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      const user = {
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      };
-  
-      const mailOptions = {
-        from: '0923riteshkumar@gmail.com',
-        to: user.email,
-        subject: 'Registration Successful',
-        text: 'Thank you for registering on our website.'
-      };
-  
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent ');
-        }
-      });
-  
-      users.push(user);
-  
-      res.redirect('/login');
-    } catch {
-      res.redirect('/register');
-    }
-  });
-  
-
-//   app.delete('/logout', (req, res) => {
-//     req.logOut()
-//     res.redirect('/login')
-//   })
+app.post('/register', checkNotAuthenticated, authController.registerUser);
 
 app.delete('/logout', (req, res) => {
-    req.logOut((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect('/login');
-    });
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/login');
   });
-
-  function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    res.redirect('/login')
-  }
-  
-  function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/')
-    }
-    next()
-  }
-
-app.listen(3000 , () => {
-    console.log('Server is running on port 3000.');
 });
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  next();
+}
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000.');
+});
+
