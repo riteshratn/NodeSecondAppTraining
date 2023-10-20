d3412893e484d20aa442ec2bb5052bc17e848034
Ritesh Kumar <0923riteshkumar@gmail.com>
2023-10-20 13:46:38 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,8 @@
 
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.get('/', userController.checkAuthenticated, userController.showIndex);
+
+module.exports = router;
