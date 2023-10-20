266f7bd2bf17ad5cb037b68a2deeb9e16390acff
Ritesh Kumar <0923riteshkumar@gmail.com>
2023-10-20 13:45:48 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,11 @@
-r
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.get('/login', authController.showLogin);
+router.post('/login', authController.loginUser);
+router.get('/register', authController.showRegister);
+router.post('/register', authController.registerUser);
+router.delete('/logout', authController.logoutUser);
+
+module.exports = router;
